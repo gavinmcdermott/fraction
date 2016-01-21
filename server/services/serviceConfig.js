2a3b6d74abae870a62a9da17895b2694504a067d
Gavin McDermott <gavinmcdermott@gmail.com>
2016-01-20 23:46:01 -0800

    remove auth service

--- server/services/serviceConfig.js
+++ server/services/serviceConfig.js
@@ -1,7 +1,6 @@
 'use strict';
 
 export const services = {
-  // "auth": { "loadPath": "./auth/authService" },
   "document": { "loadPath": "./document/documentService" },
   "property": { "loadPath": "./property/propertyService" },
   "user": { "loadPath": "./user/userService" }
